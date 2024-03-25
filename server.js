aa18e9bb006a101851e63e37ba3b7e0a307f4d8c
NonsensicalQuagga <alvin.hogdal@elev.ga.ntig.se>
2024-03-25 13:13:46 +0100

    might work

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3000
 
 const app = express()
 
-app.use(express.static("public"))
+app.use(express.static("docs"))
 
 app.use((req, res, next) => {
   res.locals.url = req.originalUrl
